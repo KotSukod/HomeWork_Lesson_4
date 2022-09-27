e9a8da99cedacdb7f21f6d79a0130cb8f2239ba7
KotSukod <XaoS.07@mail.ru>
2022-09-28 00:42:31 +1000

    задание 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿string []array = new string [8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите элемент массива': ");
+    array[i] = Console.ReadLine();
+}
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.Write("]");
