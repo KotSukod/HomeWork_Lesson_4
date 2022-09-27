13056bed082809ac27139bb13b1e5f27e542788c
KotSukod <XaoS.07@mail.ru>
2022-09-28 00:01:52 +1000

    задание 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 10;
+int result = 0;
+int help = 0;
+while (num > 0)
+{
+    help = num / count;
+    if (help > 10) count = count * 10;
+
+    else if (help < 10)
+    {
+        result = result + help;
+        num = num - help * count;
+        count = 1;
+        help = 0;
+    }
+     
+}
+Console.WriteLine($"Сумма всеx чисел равна - {result}");
+
+   
+
+
